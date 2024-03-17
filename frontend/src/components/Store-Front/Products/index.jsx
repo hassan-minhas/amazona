864e6211b768d0ab0279222a0ddeeb0027d990c2
irtazafayaz <irtazab135@gmail.com>
2024-03-17 17:30:36 +0500

    ui changes

--- frontend/src/components/Store-Front/Products/index.jsx
+++ frontend/src/components/Store-Front/Products/index.jsx
@@ -1,20 +1,75 @@
 import React from "react";
+import { useEffect, useReducer } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import axios from "axios";
+import logger from "use-reducer-logger";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Product from "../../Product";
+import LoadingBox from "../../LoadingBox";
+import MessageBox from "../../MessageBox";
 
 const trendingProducts = [
-    {
-      id: 1,
-      name: "Leather Long Wallet",
-      color: "Natural",
-      price: "$75",
-      href: "#",
-      imageSrc:
-        "https://tailwindui.com/img/ecommerce-images/home-page-04-trending-product-02.jpg",
-      imageAlt: "Hand stitched, orange leather long wallet.",
-    },
-    // More products...
-  ];
+  {
+    id: 1,
+    name: "Leather Long Wallet",
+    color: "Natural",
+    price: "$75",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/home-page-04-trending-product-02.jpg",
+    imageAlt: "Hand stitched, orange leather long wallet.",
+  },
+  // More products...
+];
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, products: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, error: action.payload, loading: false };
+    case "FETCH_SELLER_REQUEST":
+      return { ...state, loadingSellers: true };
+    case "FETCH_SELLER_SUCCESS":
+      return { ...state, sellers: action.payload, loadingSellers: false };
+    case "FETCH_SELLER_FAIL":
+      return { ...state, error: action.payload, loadingSellers: false };
+
+    default:
+      return state;
+  }
+};
 
 export const StoreFrontProducts = () => {
+  const [{ loading, error, products, loadingSellers, sellers }, dispatch] =
+    useReducer(logger(reducer), {
+      products: [],
+      sellers: [],
+      loading: true,
+      error: "",
+    });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        dispatch({ type: "FETCH_REQUEST" });
+        const result = await axios.get("/api/products");
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+
+        dispatch({ type: "FETCH_SELLER_REQUEST" });
+        const sellers = await axios.get("/api/users/top-sellers");
+        dispatch({ type: "FETCH_SELLER_SUCCESS", payload: sellers.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
+        dispatch({ type: "FETCH_SELLER_FAIL", payload: err.message });
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <section aria-labelledby="trending-heading">
       <div className="mx-auto max-w-7xl px-4 py-24 sm:px-6 sm:py-32 lg:px-8 lg:pt-32">
@@ -34,28 +89,20 @@ export const StoreFrontProducts = () => {
           </a>
         </div>
 
-        <div className="mt-6 grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-0 lg:gap-x-8">
-          {trendingProducts.map((product) => (
-            <div key={product.id} className="group relative">
-              <div className="h-56 w-full overflow-hidden rounded-md group-hover:opacity-75 lg:h-72 xl:h-80">
-                <img
-                  src={product.imageSrc}
-                  alt={product.imageAlt}
-                  className="h-full w-full object-cover object-center"
-                />
-              </div>
-              <h3 className="mt-4 text-sm text-gray-700">
-                <a href={product.href}>
-                  <span className="absolute inset-0" />
-                  {product.name}
-                </a>
-              </h3>
-              <p className="mt-1 text-sm text-gray-500">{product.color}</p>
-              <p className="mt-1 text-sm font-medium text-gray-900">
-                {product.price}
-              </p>
-            </div>
-          ))}
+        <div className="products">
+          {loading ? (
+            <LoadingBox />
+          ) : error ? (
+            <MessageBox variant="danger">{error}</MessageBox>
+          ) : (
+            <Row>
+              {products.map((product) => (
+                <Col xs={6} md={4} lg={3} key={product.slug} className="mb-3">
+                  <Product product={product} />
+                </Col>
+              ))}
+            </Row>
+          )}
         </div>
 
         <div className="mt-8 text-sm md:hidden">
